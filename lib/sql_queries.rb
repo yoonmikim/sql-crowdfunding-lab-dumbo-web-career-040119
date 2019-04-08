3d566dd87359b67c153d787d513dedec56b4c3d9
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-08 20:01:51 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,8 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 join users
 on users.id = pledges.user_id 
 Group by users.name
-order by pledges.amount;"
+order by pledges.amount
+order by users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
