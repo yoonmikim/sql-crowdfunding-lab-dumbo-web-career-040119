615de9c1ee01cf16d886be34e656d101a1aecb9e
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-08 20:06:57 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,25 +7,47 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) as pledges_amount from pledges
+join projects
+on projects.id = pledges.project_id 
+Group by projects.title
+order by projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount) as pledges_amount from pledges
+join users
+on users.id = pledges.user_id 
+Group by users.name
+order by users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) as success from pledges
+join projects
+on projects.id = pledges.project_id 
+Group by projects.title
+having success >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) as pledges_amount from pledges
+join users
+on users.id = pledges.user_id 
+Group by users.name
+order by pledges_amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount as pledges_amount from pledges
+join projects
+on projects.id = pledges.project_id 
+WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) as pledges_amount from pledges
+join projects
+on projects.id = pledges.project_id 
+WHERE projects.category = 'books';"
 end
