a52d0543a46b1d231dae6894ea4d9a3d5f8e9876
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-08 19:36:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, SUM(pledges.amount) as pledges_amount from pledges
 join projects
 on projects.id = pledges.project_id 
-Group by projects.title;"
+Group by projects.title
+order by projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
