e8397a31bad2c5082db63e0d43936f9ccb3476b7
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-08 19:57:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,11 @@ order by users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) as success as pledges_amount from pledges
+join projects
+on projects.id = pledges.project_id 
+Group by projects.title
+having success >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
