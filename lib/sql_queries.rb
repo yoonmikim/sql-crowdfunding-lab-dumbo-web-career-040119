ac3410a4a9a70a64e940b03c8af508b9ed92ed9f
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-08 19:33:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,10 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, pledges.amount from pledges"
+"SELECT projects.title, SUM(pledges.amount) as pledges_amount from pledges
+join projects
+on projects.id = pledges.project 
+Group by projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
